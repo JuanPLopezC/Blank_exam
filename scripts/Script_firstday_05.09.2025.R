0376ee910705a8119984b06c0119e31555c8a6d7
Guri Kringeland <guri.kringeland@gmail.com>
2025-09-08 11:20:01 +0200

    tidying the script through two stages

--- scripts/Script_firstday_05.09.2025.R
+++ scripts/Script_firstday_05.09.2025.R
@@ -1,13 +1,23 @@
 #MAIN SCRIPT FOR BLANK EXAM - STARTING 05-09-2025#
 
-ata <- read_delim(here("data", "exam_data.txt"))
+library(dplyr)
+library (usethis)
+library(here)
+library(tidyverse)
+library(skimr)
+
+use_git_config(fetch.prune = "true")
+here("data", "exam_data.txt")
+
+#exploring the data
+data <- read_delim(here("data", "exam_data.txt"))
 skim(data)                  
 summary(data)
 head(data)
 tail(data)
 glimpse(data)
 gg_miss_var(data)
-library(dplyr)
+
 
 data %>% count(subject)
 any(duplicated(data))
@@ -17,9 +27,14 @@ sum(duplicated(data$subject))
 names(data)
 
 
-
+#separate collum subject til Hosp og ID
 data <-data %>% 
   separate(col =subject, 
            into = c("Hosp", "ID"), 
            sep = "-")
 
+#Removing duplicates. Checking afterwars that the clean_data has 40 less rows. 
+data_clean<- data %>%
+  distinct()
+nrow(data_clean)
+nrow(data)
