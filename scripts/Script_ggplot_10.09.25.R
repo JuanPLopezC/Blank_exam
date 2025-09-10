56e21a49419be15e9a470ecd9b82cfb302c22c5d
Juan Pablo Lopez <juan.cervantes@uib.no>
2025-09-10 15:24:39 +0200

    beginning with the script

--- scripts/Script_ggplot_10.09.25.R
+++ scripts/Script_ggplot_10.09.25.R
@@ -1,3 +1,15 @@
 #Script for fifth day - ggplot 10.09.09#
 
 
+#library(here)
+#library(tidyverse)
+#library(patchwork)
+
+
+##Reading the ggplot version from 10.09.2025----
+
+ggplot_exam_data <- read_delim("data/ggplot_exam_data_2025-09-10.txt", 
+                             delim = "\t", escape_double = FALSE, 
+                             trim_ws = TRUE)
+
+ggplot_exam_data
