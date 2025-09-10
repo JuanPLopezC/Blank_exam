eff90654028d3255a2ff65d6b578528a52a34d9e
Juan Pablo Lopez <juan.cervantes@uib.no>
2025-09-10 10:56:50 +0200

    starting with my part of stratification

--- scripts/Script_fourthday_10.09.2025.R
+++ scripts/Script_fourthday_10.09.2025.R
@@ -2,6 +2,15 @@
 
 #We had to do more tidy on the data because we found there were some wrong variables, we started using the joint_exam_data
 
-tidy_exam_data <- read_delim("data/joint_exam_data_2025-09-09.txt", 
+joint_exam_data <- read_delim("data/joint_exam_data_2025-09-09.txt", 
                              delim = "\t", escape_double = FALSE, 
                              trim_ws = TRUE)
+
+
+
+#Doing some of the exercises about stratifying#
+#Stratify your data by a categorical column and report min, max, mean and sd of a numeric column for a defined set of observations - use a pipe `%>%`!    
+#- Only for persons with `T.Stage == 1`
+#- Only for persons with `Median.RBC.Age == 25`
+
+
