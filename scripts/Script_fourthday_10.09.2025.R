71bcb3b55252efbcdcb42bae4315b0e4830a3658
Juan Pablo Lopez <juan.cervantes@uib.no>
2025-09-10 11:13:02 +0200

    last changes in the script

--- scripts/Script_fourthday_10.09.2025.R
+++ scripts/Script_fourthday_10.09.2025.R
@@ -1,26 +1,13 @@
 #Script fourth day 10.09.2025#
 
-#We had to do more tidy on the data because we found there were some wrong variables, we started using the joint_exam_data
-
-joint_exam_data <- read_delim("data/joint_exam_data_2025-09-09.txt", 
-                             delim = "\t", escape_double = FALSE, 
-                             trim_ws = TRUE)
-
-<<<<<<< HEAD
+#We had to do more tidy on the data because we found there were some wrong variables, we started using the joint_exam_data from 09.09.2025
 
 
-#Doing some of the exercises about stratifying#
-#Stratify your data by a categorical column and report min, max, mean and sd of a numeric column for a defined set of observations - use a pipe `%>%`!    
-#- Only for persons with `T.Stage == 1`
-#- Only for persons with `Median.RBC.Age == 25`
-
-
-=======
 #Here i try fix the timetoreccurance error - Anders  
 #This is to make the correct columns in days and weeks and round the values so that it doesn't include decimals
-tidy_exam_data <- readr::read_tsv("data/joint_exam_data_2025-09-10.txt") %>%
+tidy_exam_data <- readr::read_tsv("data/joint_exam_data_2025-09-09.txt") %>%
   mutate(
-    unit_norm = str_to_lower(str_trim(TimeToRecurrence_unit)),
+    unit_norm = str_to_lower(str_trim(TimeToRecurrence_unit)), 
     ttr_num   = readr::parse_number(as.character(TimeToRecurrence)),
     TimeToRecurrence_days_new = case_when(
       str_starts(unit_norm, "week") ~ ttr_num * 7,
@@ -37,4 +24,12 @@ tidy_exam_data <- readr::read_tsv("data/joint_exam_data_2025-09-10.txt") %>%
 #This is to delete the old column - timetoreccurance_days
 tidy_exam_data <- tidy_exam_data %>%
   select(-any_of(c("TimeToRecurrence_days", "timetoreccurance_days")))
->>>>>>> ae0e91a9b891d8f2864f97809de2d9656ec96583
+
+
+
+#Doing some of the exercises about stratifying#
+#Stratify your data by a categorical column and report min, max, mean and sd of a numeric column for a defined set of observations - use a pipe `%>%`!    
+#- Only for persons with `T.Stage == 1`
+#- Only for persons with `Median.RBC.Age == 25`
+
+
