c9e6781c1e37e705d892a3c0384f5c5d0ed048a3
Juan Pablo Lopez <juan.cervantes@uib.no>
2025-09-11 13:50:17 +0200

    changes in script after using styler

--- scripts/Script_thirdday_2025-09-09.R
+++ scripts/Script_thirdday_2025-09-09.R
@@ -1,4 +1,4 @@
-#Script day 3 - 09.09.2025#
+#Script day 3 - 2025-09-09----
 
 library(here)
 library(tidyverse)
@@ -10,19 +10,19 @@ tidy_exam_data <- read_delim("data/tidy_exam_data_2025-09-09.txt",
                              delim = "\t", escape_double = FALSE, 
                              trim_ws = TRUE)
 
-#Read additional data#
+##Read additional data#----
 additional_data <- read_delim("data/exam_data_join.txt", 
            delim = "\t", escape_double = FALSE, 
            trim_ws = TRUE)
 
-#Join the additional data#
+##Join the additional data#----
 
 joint_exam_data <- tidy_exam_data %>%
   left_join(additional_data, join_by(ID == id))
 
 glimpse(joint_exam_data)
 
-#to save it as a new database that will contain the joint data
+##To save it as a new database that will contain the joint data----
 fileName <- paste0("data/joint_exam_data_", Sys.Date(), ".txt")
 write_delim(
   joint_exam_data, 
