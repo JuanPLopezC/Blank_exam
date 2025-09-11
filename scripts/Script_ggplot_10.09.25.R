d7210e6d9927f32e9e49d2d4d98b38c3047a8338
Juan Pablo Lopez <juan.cervantes@uib.no>
2025-09-11 11:32:46 +0200

    Changes made by Guri and JP on the ggplot script

--- scripts/Script_ggplot_10.09.25.R
+++ scripts/Script_ggplot_10.09.25.R
@@ -8,16 +8,14 @@
 #library(ggcorrplot)
 #install.packages("ggplot2")
 #install.packages("ggcorrplot")
+#library(here)
 
 #starting fresh with a new name of datafile and new script for task 4, concerning ggplot. 
 ggplot_exam_data <- read_tsv("data/ggplot_exam_data_2025-09-10.txt")
 
 
-#Exercise: Are there any correlated measurements? Yes, see plot below - Anders
-ggplot_data <- read_tsv("data/ggplot_exam_data_2025-09-10.txt")
-
 ## Choosing solely numeric columns and filtrating columns that causes issues (missing N/A, or ID)
-num <- ggplot_data %>%
+num <- ggplot_exam_data %>%
   select(where(is.numeric)) %>%                                
   select(-any_of(c("ID", "ttr_num", "TimeToRecurrence_weeks_new"))) %>%  
   mutate(across(everything(), ~ ifelse(is.finite(.), ., NA_real_))) %>%  
@@ -52,12 +50,12 @@ plot_1 <- ggcorrplot(
 plot_1
 
 #Exercise: Is there a relation between the `PVol` and `TVol` variables? Yes, there is a weak negative association between PVol and TVol (Spearman ρ = −0.21, p < 0.001). Anders 
-ggplot_exam_data <- ggplot_data %>%                   
+ggplot_exam_data2 <- ggplot_exam_data %>%                   
   select(PVol, TVol) %>%
   filter(!is.na(PVol), !is.na(TVol))
 
 #Checking for correlation
-spear <- cor.test(ggplot_exam_data$PVol, ggplot_exam_data$TVol, method = "spearman")
+spear <- cor.test(ggplot_exam_data2$PVol, ggplot_exam_data2$TVol, method = "spearman")
 
 spear
 
@@ -81,21 +79,21 @@ plot_2
 
 #Adding Spearman correlation to the boxplot
 sp <- cor.test(dat$PVol, as.numeric(dat$TVol), method = "spearman")
-plot_2 + annotate("text",
+plot_2a <- plot_2 + annotate("text",
              x = Inf, y = Inf,
              label = sprintf("Spearman rho = %.2f, p = %.3g",
                              sp$estimate, sp$p.value),
              hjust = 1.05, vjust = 1.5, size = 4)
 
-plot_2
+plot_2a
 
 #Exercise: Does the distribution of `Preoperativte_PSA` differ by `Tumor_stage`? yes, see plot. 
 plot_3 <- ggplot(ggplot_exam_data, aes(x = factor(Tumor_stage), y = Preoperative_PSA)) +
   geom_boxplot(fill = "lightblue", color = "darkblue") +
   labs(
-    title = "Distribusjon av Preoperative PSA etter Tumor stage",
+    title = "Distribution of Preoperative PSA by Tumor stage",
     x = "Tumor stage",
-    y = "Preoperativ PSA"
+    y = "Preoperative PSA"
   ) +
   theme_minimal()
 summary(ggplot_exam_data$Preoperative_PSA)
@@ -107,7 +105,7 @@ plot_3
 plot_4 <- ggplot(ggplot_exam_data, aes(x = factor(sGS), y = PVol)) +
   geom_boxplot(fill = "lightblue", color = "darkblue") +
   labs(
-    title = "Distribusjon av Protate Volum etter surgical Gleason score",
+    title = "Distribustion of Protate Volume by surgical Gleason score",
     x = "Surgical Gleason Score",
     y = "Prostate Volume"
   ) +
@@ -123,33 +121,35 @@ summary(ggplot_exam_data$sGS)
 plot_5 <- ggplot(ggplot_exam_data, aes(x = factor(sGS), y = TVol)) +
   geom_boxplot(fill = "lightblue", color = "darkblue") +
   labs(
-    title = "Distribusjon av Tumor Volum etter surgical Gleason score",
+    title = "Distribution of Tumor Volume by surgical Gleason score",
     x = "Surgical Gleason Score",
     y = "Tumor Volume"
   ) +
   theme_minimal()
 
+plot_5
+
 summary(ggplot_exam_data$TVol)
 summary(ggplot_exam_data$sGS)
 
-plot_5
+
 
 #Does the distribution of `TVol` differ by `sGS`?  
 # both TVOl and sGs are categorical, not continues variables,therefor choosing grouped bar plot for this task. 
 #there are quite a few NA for postopr gleasonscore ( is it like this in real life?), so I choose to keep the values. 
 
-plot_6 <- ggplot_exam_data %>%
+plot_5b <- ggplot_exam_data %>%
   ggplot(aes(x = factor(TVol), fill = factor(sGS))) +
   geom_bar(position = "dodge") +
   labs(
-    title = "Fordeling av Tumor volum etter gleason score",
-    x = "Tumor_volum",
-    y = "Antall",
-    fill = "sGS (postoperativ Gleason score)"
+    title = "Distribution of Tumor volume by Gleason score",
+    x = "Tumor volume",
+    y = "Number of patients",
+    fill = "sGS (postoperative Gleason score)"
   ) +
   theme_minimal()
 
-plot_6
+plot_5b
 
 # Task: Where there more `T.Stage == 2` in the group with `PreopTherapy == 1` than in the group `PreopTherapy == 0`?
 
